c347681f1c0db1ad621662da3b84fb00368a6ff0
Alexander Gribkov <gribkovalexander@gmail.com>
2024-01-25 01:26:32 +0300

    Problem 189

---
+++ src/kotlincode/Solution189.kt
@@ -0,0 +1,19 @@
+package kotlincode
+
+class Solution189 {
+    fun rotate(nums: IntArray, k: Int): Unit {
+        val point = k % nums.size
+        with(nums) {
+            reverse(0, nums.size)
+            reverse(0, point)
+            reverse(point, nums.size)
+        }
+    }
+}
+
+fun main() {
+    val nums = intArrayOf(1,2,3,4,5,6,7)
+    val solution = Solution189()
+    solution.rotate(nums, 3)
+    nums.forEach { println(it) }
+}

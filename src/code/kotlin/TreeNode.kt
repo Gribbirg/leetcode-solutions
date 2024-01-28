f89306793c7134ca1b822f70ea9db1ff1712f778
Alexander Gribkov <gribkovalexander@gmail.com>
2024-01-28 22:50:52 +0300

    Tree constructor

--- src/code/kotlin/TreeNode.kt
+++ src/code/kotlin/TreeNode.kt
@@ -1,6 +1,37 @@
 package code.kotlin
 
+import java.util.LinkedList
+import java.util.Queue
+
 class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
+
+
+    constructor(array: ArrayList<Int?>) : this(array[0]!!) {
+        val queue: Queue<TreeNode> = LinkedList()
+        queue.add(this)
+
+        val iterator = array.iterator()
+        iterator.next()
+        while (iterator.hasNext()) {
+            var x = iterator.next()
+            val node = queue.poll()
+
+            x?.let {
+                node.left = TreeNode(it)
+                queue.add(node.left)
+            }
+
+            if (iterator.hasNext()) {
+                x = iterator.next()
+                x?.let {
+                    node.right = TreeNode(it)
+                    queue.add(node.right)
+                }
+            } else {
+                break
+            }
+        }
+    }
 }

3261fe179b6e1f119fbd09263b0804c8a0dc6ef4
Alexander Gribkov <gribkovalexander@gmail.com>
2024-02-07 00:30:15 +0300

    Is Subsequence

---
+++ src/code/dart/IsSubsequence.dart
@@ -0,0 +1,18 @@
+class IsSubsequence {
+  bool isSubsequence(String s, String t) {
+    var j = 0;
+    for (var i = 0; i < s.length; i++) {
+      for(;j < t.length && s[i] != t[j]; j++);
+      if (j == t.length) return false;
+      j++;
+    }
+    return true;
+  }
+}
+
+main() {
+  var sol = IsSubsequence();
+  print(sol.isSubsequence("abc", "ahbgdc"));
+  print(sol.isSubsequence("axc", "ahbgdc"));
+  print(sol.isSubsequence("", ""));
+}

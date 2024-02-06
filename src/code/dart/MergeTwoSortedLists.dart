2297f86b58f50a5e22f2f07af2e02bafa9a2349c
Alexander Gribkov <gribkovalexander@gmail.com>
2024-02-06 18:36:08 +0300

    Merge Two Sorted Lists

---
+++ src/code/dart/MergeTwoSortedLists.dart
@@ -0,0 +1,50 @@
+import 'structures/ListNode.dart';
+
+class MergeTwoSortedLists {
+  ListNode? mergeTwoLists(ListNode? list1, ListNode? list2) {
+    if (list1 == null) return list2;
+    if (list2 == null) return list1;
+
+    ListNode? node1 = list1;
+    ListNode? node2 = list2;
+    ListNode resNode;
+
+    if (node1.val <= node2.val) {
+      resNode = node1;
+      node1 = node1.next;
+    } else {
+      resNode = node2;
+      node2 = node2.next;
+    }
+
+    ListNode? resList = resNode;
+
+    while (node1 != null && node2 != null) {
+      if (node1.val <= node2.val) {
+        resNode.next = node1;
+        resNode = node1;
+        node1 = node1.next;
+      } else {
+        resNode.next = node2;
+        resNode = node2;
+        node2 = node2.next;
+      }
+    }
+
+    if (node1 != null) resNode.next = node1;
+    if (node2 != null) resNode.next = node2;
+
+    return resList;
+  }
+}
+
+main() {
+  print(MergeTwoSortedLists().mergeTwoLists(
+      ListNode(1, ListNode(2, ListNode(4))),
+      ListNode(1, ListNode(3, ListNode(4)))));
+  print(MergeTwoSortedLists().mergeTwoLists(
+      ListNode(1, ListNode(2, ListNode(4))),
+      ListNode(1, ListNode(8, ListNode(9)))));
+  print(MergeTwoSortedLists().mergeTwoLists(null, null));
+  print(MergeTwoSortedLists().mergeTwoLists(null, ListNode(0)));
+}

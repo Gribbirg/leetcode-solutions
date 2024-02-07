f7086a35c15857d97eaef906450be2e53a50b92c
Alexander Gribkov <gribkovalexander@gmail.com>
2024-02-07 23:35:22 +0300

    Factorial Trailing Zeroes

---
+++ src/code/dart/FactorialTrailingZeroes.dart
@@ -0,0 +1,17 @@
+class FactorialTrailingZeroes {
+  int trailingZeroes(int n) {
+    var res = 0;
+    for (var div = 5; div <= n; div *= 5) {
+      res += n ~/ div;
+    }
+    return res;
+  }
+}
+
+main() {
+  var sol = FactorialTrailingZeroes();
+  print(sol.trailingZeroes(3));
+  print(sol.trailingZeroes(5));
+  print(sol.trailingZeroes(0));
+  print(sol.trailingZeroes(20));
+}

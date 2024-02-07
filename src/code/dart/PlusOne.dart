1f8fcc621a833acff46fcd5407457281b5ad1eb9
Alexander Gribkov <gribkovalexander@gmail.com>
2024-02-07 15:50:57 +0300

    Plus One

---
+++ src/code/dart/PlusOne.dart
@@ -0,0 +1,20 @@
+class PlusOne {
+  List<int> plusOne(List<int> digits) {
+    int sum;
+    var i = digits.length - 1;
+    do {
+      sum = ++digits[i];
+      digits[i] = sum % 10;
+      i--;
+    } while (sum == 10 && i >= 0);
+    if (sum == 10) digits.insert(0, 1);
+    return digits;
+  }
+}
+
+main() {
+  var sol = PlusOne();
+  print(sol.plusOne([1, 2, 3]));
+  print(sol.plusOne([4, 3, 2, 1]));
+  print(sol.plusOne([9]));
+}

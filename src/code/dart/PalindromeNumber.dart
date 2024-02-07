7900a96158c90d5e56e6d76e7bac11f8de7c7eca
Alexander Gribkov <gribkovalexander@gmail.com>
2024-02-07 15:35:34 +0300

    Palindrome Number

---
+++ src/code/dart/PalindromeNumber.dart
@@ -0,0 +1,19 @@
+class PalindromeNumber {
+  bool isPalindrome(int x) {
+    if (x < 0) return false;
+    int n = x;
+    int rev = 0;
+    while (n > 0) {
+      rev = rev * 10 + n % 10;
+      n ~/= 10;
+    }
+    return rev == x;
+  }
+}
+
+main() {
+  var sol = PalindromeNumber();
+  print(sol.isPalindrome(121));
+  print(sol.isPalindrome(-121));
+  print(sol.isPalindrome(10));
+}

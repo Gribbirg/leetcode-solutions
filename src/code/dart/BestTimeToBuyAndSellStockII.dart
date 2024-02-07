adf7563119b57c1450f67939264e8f27d62968b6
Alexander Gribkov <gribkovalexander@gmail.com>
2024-02-07 23:59:45 +0300

    Best Time to Buy and Sell Stock II

---
+++ src/code/dart/BestTimeToBuyAndSellStockII.dart
@@ -0,0 +1,19 @@
+class BestTimeToBuyAndSellStockII {
+  int maxProfit(List<int> prices) {
+    var profit = 0;
+
+    for (int i = 1; i < prices.length; i++) {
+      int dif = prices[i] - prices[i - 1];
+      if (dif > 0) profit += dif;
+    }
+
+    return profit;
+  }
+}
+
+main() {
+  var sol = BestTimeToBuyAndSellStockII();
+  print(sol.maxProfit([7, 1, 5, 3, 6, 4]));
+  print(sol.maxProfit([1, 2, 3, 4, 5]));
+  print(sol.maxProfit([7, 6, 4, 3, 1]));
+}

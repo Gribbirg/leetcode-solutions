68fc1daf7bf18cb2c2fda8f6b22c01e17a5b39ad
Alexander Gribkov <gribkovalexander@gmail.com>
2024-02-07 23:43:39 +0300

    Best Time to Buy and Sell Stock

---
+++ src/code/dart/BestTimeToBuyAndSellStock.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+class BestTimeToBuyAndSellStock {
+  int maxProfit(List<int> prices) {
+    var maxProfit = 0;
+    var minValue = prices.first;
+    for (var i = 1; i < prices.length; i++) {
+      if (prices[i] > minValue)
+        maxProfit = max(maxProfit, prices[i] - minValue);
+      else
+        minValue = prices[i];
+    }
+    return maxProfit;
+  }
+}
+
+main() {
+  var sol = BestTimeToBuyAndSellStock();
+  print(sol.maxProfit([7, 1, 5, 3, 6, 4]));
+  print(sol.maxProfit([7, 6, 4, 3, 1]));
+}

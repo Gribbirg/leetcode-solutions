a234fc1262564a525869ee089cd4502cca045625
Alexander Gribkov <gribkovalexander@gmail.com>
2024-02-09 15:53:51 +0300

    Jump Game

---
+++ src/code/dart/JumpGame.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+class JumpGame {
+  bool canJump(List<int> nums) {
+    var reach = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (i > reach) return false;
+      reach = max(reach, i + nums[i]);
+    }
+    return true;
+  }
+}
+
+void main() {
+  var sol = JumpGame();
+  print(sol.canJump([2, 3, 1, 1, 4]));
+  print(sol.canJump([3, 2, 1, 0, 4]));
+}

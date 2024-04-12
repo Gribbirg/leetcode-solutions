d8d86467eeaed465665293d325ac0cc76d847262
Alexander Gribkov <gribkovalexander@gmail.com>
2024-04-12 20:56:43 +0300

    71. Simplify Path

---
+++ src/code/kotlin/SimplifyPath.kt
@@ -0,0 +1,32 @@
+package code.kotlin
+
+import java.util.*
+
+class SimplifyPath {
+    fun simplifyPath(path: String): String {
+        val list = mutableListOf<String>()
+
+        path.split("/").forEach { s ->
+            if (s.isEmpty() || s == ".") {
+                return@forEach
+            }
+
+            if (s == "..") {
+                if (list.isNotEmpty())
+                    list.removeLast()
+                return@forEach
+            }
+
+            list.add(s)
+        }
+
+        return "/" + list.joinToString("/")
+    }
+}
+
+fun main() {
+    val sol = SimplifyPath()
+    println(sol.simplifyPath("/home/"))
+    println(sol.simplifyPath("/../"))
+    println(sol.simplifyPath("/home//foo/"))
+}
